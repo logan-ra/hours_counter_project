40e301f67fe4ef8a4084957a62311fc883d8cdc2
logan <loganwrains@gmail.com>
2022-03-31 14:19:40 -0400

    Turned path variable into an input so whoever is using the program can enter their specific file path to their desk schedule folder.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import openpyxl
 import os
 from Hours_counter import hours_counter
 
-path = 'C:/Users/lrains/Desktop/CodeKY/desk schedules'
+path = input("Enter desk schedule folder filepath: ")
 
 obj = os.scandir(path)
 
